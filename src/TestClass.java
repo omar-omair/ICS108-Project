67db1f98ed5638bbb596d6529b3bd5f05d2cfba0
omar-omair <omaromair123@gmail.com>
2021-11-19 17:31:00 +0300

    first

---
+++ src/TestClass.java
@@ -0,0 +1,12 @@
+// Just a test class to check whether the data is read or not.
+
+public class TestClass {
+	 
+	public static void main(String[] args) {
+		CommonClass.loadBinaryData();
+		
+	}
+	
+  
+
+}
