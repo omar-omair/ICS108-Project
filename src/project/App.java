268db4954e9fd90a56f36874efedb62c198635ad
omar-omair <87987939+omar-omair@users.noreply.github.com>
2021-11-20 23:04:12 +0300

    add implemtaion of readobject

--- src/project/App.java
+++ src/project/App.java
@@ -20,8 +20,18 @@ import java.io.*;
 
 public class App extends Application{
     private Label registerSystem = new Label("Registration System");
-    
-    
+    TextField courseID = new TextField();
+    TextField courseName = new TextField();
+    TextField courseDays = new TextField();
+    TextField courseLocation = new TextField();
+    TextField courseTime = new TextField();
+    TextField courseStatus = new TextField();
+    ArrayList<Course> coursesList = new ArrayList<Course>();
+    ArrayList<Student> studentsList = new ArrayList<Student>();
+    ListView<String> studentList = new ListView<>();
+    Pane panel = new Pane();
+    Label numberLabel = new Label("");
+
     public void start(Stage stage) throws Exception{
         registerSystem.setFont(new Font(40));
         BorderPane borderPane = new BorderPane();
@@ -36,22 +46,23 @@ public class App extends Application{
         borderPane.setBottom(buttonBox);
         BorderPane.setMargin(buttonBox, new Insets(12,12,70,12));
         Scene main = new Scene(borderPane);
-        ArrayList<Course> coursesList = new ArrayList<Course>();
+        
+        
+        
         FileInputStream fis = new FileInputStream("res\\Registration.dat");
         ObjectInputStream ois = new ObjectInputStream(fis);
         coursesList = (ArrayList<Course>) ois.readObject();
-        
-        Pane panel = new Pane();
+        studentsList = (ArrayList<Student>) ois.readObject();
         Scene courses = new Scene(panel);
         ListView<Course> courseList = new ListView<>(FXCollections.observableArrayList(coursesList));
-        ListView studentsList = new ListView();
+        courseList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         GridPane details = new GridPane();
         details.setHgap(10);
         details.setVgap(5);
         Label[] labels = {new Label("ID"),new Label("Name"),
                          new Label("Days"),new Label("Location"), new Label("Time"),
                          new Label("Status")};
-
+        TextField[] fields = {courseID,courseName,courseDays,courseLocation,courseTime,courseStatus};
         Button back = new Button("Back");
         Button pervious = new Button("< Pervious");
         Button next = new Button("Next >");
@@ -60,19 +71,54 @@ public class App extends Application{
         HBox buttonHbox = new HBox(10);
         for(int i=0; i<labels.length; i++){
            details.addColumn(0, labels[i]);
-           details.addColumn(1, new TextField());
+           details.addColumn(1, fields[i]);
+           fields[i].setPrefWidth(270);
            if(i <= 3)
             buttonHbox.getChildren().add(buttons[i]);
         }
-        panel.getChildren().addAll(details,courseList,studentsList,buttonHbox);
-        details.setLayoutX(320);
+        
+        panel.getChildren().addAll(details,courseList,studentList,buttonHbox,numberLabel);
+        studentList.setPrefHeight(350);
+        details.setLayoutX(280);
         details.setLayoutY(100);
-        studentsList.setLayoutX(630);
-        studentsList.setLayoutY(10);
+        studentList.setLayoutX(630);
+        studentList.setLayoutY(58);
         courseList.setLayoutX(10);
         courseList.setLayoutY(10);
         buttonHbox.setLayoutY(430);
         buttonHbox.setLayoutX(300);
+        numberLabel.setLayoutX(630);
+        numberLabel.setLayoutY(38);
+
+        courseList.getSelectionModel().selectedItemProperty().addListener(t -> {
+            int courseIndex = courseList.getSelectionModel().getSelectedIndex();
+            courseID.setText(coursesList.get(courseIndex).getCourseID());
+            courseName.setText(coursesList.get(courseIndex).getCourseName());
+            courseDays.setText(coursesList.get(courseIndex).getCourseDays());
+            courseTime.setText(coursesList.get(courseIndex).getCourseTime());
+            courseLocation.setText(coursesList.get(courseIndex).getCourseLocation());
+            if(coursesList.get(courseIndex).getAvailableSeats() == 0) {
+                courseStatus.setText("Closed");
+            }
+            else {
+                courseStatus.setText("Open");
+            }
+
+            ArrayList<String> registered = new ArrayList<String>();
+            int count = 0;
+            for(int i = 0; i< studentsList.size(); i++) {
+                for(int j =0; j < studentsList.get(i).getCourses().size(); j++) {
+                    if(studentsList.get(i).getCourses().get(j).getCourseID().equals(courseID.getText())) {
+                        registered.add(count, studentsList.get(i).getStudID());
+                        count++;
+                    }
+                }
+                
+            }
+            numberLabel.setText("There are " + (count+1) + " students registered in " + courseID.getText());
+            studentList.setItems(FXCollections.observableArrayList(registered));
+        });
+
 
 
         GridPane grid = new GridPane();
@@ -103,6 +149,7 @@ public class App extends Application{
             stage.setScene(courses);
             stage.setWidth(900);
             stage.setHeight(500);
+            stage.setTitle("Courses");
         });
 
         student.setOnAction(e -> {
@@ -115,12 +162,24 @@ public class App extends Application{
             stage.setScene(main);
             stage.setWidth(700);
             stage.setHeight(600);
+            stage.setTitle("main");
+            ArrayList<String> empty = new ArrayList<String>();
+            courseList.getSelectionModel().clearSelection();
+            courseID.setText("");
+            courseName.setText("");
+            courseDays.setText("");
+            courseTime.setText("");
+            courseStatus.setText("");
+            courseLocation.setText("");
+            numberLabel.setText("");
+            studentList.setItems(FXCollections.observableArrayList(empty));
         });
 
         back2.setOnAction(e -> {
             stage.setScene(main);
             stage.setWidth(700);
             stage.setHeight(600);
+            stage.setTitle("main");
         });
 
         stage.setScene(main);
